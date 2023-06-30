2b0713152e0384229941649dbe502f9189f7c21d
thalassa_home <blinov.gosha@mail.ru>
2023-06-30 19:04:40 +0300

    downgrade to javax

--- src/main/java/crud/app/models/Member.java
+++ src/main/java/crud/app/models/Member.java
@@ -1,10 +1,10 @@
 package crud.app.models;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import java.util.List;
 
