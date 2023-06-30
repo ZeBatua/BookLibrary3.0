ca893a21c90fd577fe6c2f2a9df0809f05403dc9
thalassa_home <blinov.gosha@mail.ru>
2023-06-30 20:29:22 +0300

    add AuthController

---
+++ src/main/java/crud/app/controllers/AuthController.java
@@ -0,0 +1,52 @@
+package crud.app.controllers;
+
+import crud.app.models.Member;
+import crud.app.util.MemberValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final MemberValidator memberValidator;
+    private final RegistrationService registrationService; // TODO
+
+    @Autowired
+    public AuthController(MemberValidator memberValidator, RegistrationService registrationService) {
+        this.memberValidator = memberValidator;
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("member") Member member) {
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("member") @Valid Member member,
+                                      BindingResult bindingResult) {
+        memberValidator.validate(member, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "/auth/registration";
+
+        registrationService.register(member);
+
+        return "redirect:/auth/login";
+    }
+
+
+}
