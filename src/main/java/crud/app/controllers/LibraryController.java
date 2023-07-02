5eae5353ab8e54d40e6855042e6edd929ad5c026
thalassa_home <blinov.gosha@mail.ru>
2023-07-02 16:33:01 +0300

    del debug things

--- src/main/java/crud/app/controllers/LibraryController.java
+++ src/main/java/crud/app/controllers/LibraryController.java
@@ -2,15 +2,12 @@ package crud.app.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LibraryController {
     @GetMapping("/library")
-    public String mainPage(Model model) {
-        System.out.println("hello1");
-        model.addAttribute("myString", "123");
+    public String mainPage() {
         return "/library/mainPage";
     }
 }
